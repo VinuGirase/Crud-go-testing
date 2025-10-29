a24014d43ab9cc85b41698abe635fedf657f914b
Vinu Girase <114801691+VinuGirase@users.noreply.github.com>
2025-10-29 13:14:21 +0530

    Update cohort_repository.go

--- repository/cohort_repository.go
+++ repository/cohort_repository.go
@@ -1,27 +1,63 @@
+// package repository
+
+// import (
+// 	"crud-test-go/config"
+// 	"crud-test-go/models"
+// )
+
+// func GetAllCohorts() ([]models.Cohort, error) {
+// 	var cohorts []models.Cohort
+// 	db := config.DB
+// 	result := db.Table("bnxt_user.cohort_master").Order("id desc").Find(&cohorts)
+// 	return cohorts, result.Error
+// }
+
+// func GetCohortByID(id uint) (*models.Cohort, error) {
+// 	var cohort models.Cohort
+// 	result := config.DB.Table("bnxt_user.cohort_master").First(&cohort, id)
+// 	if result.Error != nil {
+// 		return nil, result.Error
+// 	}
+// 	return &cohort, nil
+// }
+
+// func CreateCohort(cohort models.Cohort) (*models.Cohort, error) {
+// 	result := config.DB.Table("bnxt_user.cohort_master").Create(&cohort)
+// 	return &cohort, result.Error
+// }
+
 package repository
 
 import (
-	"crud-test-go/config"
 	"crud-test-go/models"
+	"gorm.io/gorm"
 )
 
-func GetAllCohorts() ([]models.Cohort, error) {
+// Wrap repository with struct
+type CohortRepository struct {
+	db *gorm.DB
+}
+
+func NewCohortRepository(db *gorm.DB) *CohortRepository {
+	return &CohortRepository{db: db}
+}
+
+func (r *CohortRepository) GetAllCohorts() ([]models.Cohort, error) {
 	var cohorts []models.Cohort
-	db := config.DB
-	result := db.Table("bnxt_user.cohort_master").Order("id desc").Find(&cohorts)
+	result := r.db.Table("bnxt_user.cohort_master").Order("id desc").Find(&cohorts)
 	return cohorts, result.Error
 }
 
-func GetCohortByID(id uint) (*models.Cohort, error) {
+func (r *CohortRepository) GetCohortByID(id uint) (*models.Cohort, error) {
 	var cohort models.Cohort
-	result := config.DB.Table("bnxt_user.cohort_master").First(&cohort, id)
+	result := r.db.Table("bnxt_user.cohort_master").First(&cohort, id)
 	if result.Error != nil {
 		return nil, result.Error
 	}
 	return &cohort, nil
 }
 
-func CreateCohort(cohort models.Cohort) (*models.Cohort, error) {
-	result := config.DB.Table("bnxt_user.cohort_master").Create(&cohort)
+func (r *CohortRepository) CreateCohort(cohort models.Cohort) (*models.Cohort, error) {
+	result := r.db.Table("bnxt_user.cohort_master").Create(&cohort)
 	return &cohort, result.Error
 }
